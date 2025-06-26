659f0879aa509fddcf7d90fe2fec230d26e2ffab
pascoalf <up202010522@g.uporto.pt>
2025-06-26 11:45:04 +0100

    Change path

--- blastn/run_blast.sh
+++ blastn/run_blast.sh
@@ -1,2 +1,3 @@
-blastn -db ../db/mito -query ../input/ASV.fasta -out ../output/blast_results -outfmt "6 delim=, qacc qlen sseqid sacc slen evalue bitscore score length pident nident mismatch positive gaps staxid ssciname sblastname scomnames"
+# Change path as necessary
+blastn -db ./db/mito -query ./ASV.fasta -out ../output/blast_results -outfmt "6 delim=, qacc qlen sseqid sacc slen evalue bitscore score length pident nident mismatch positive gaps staxid ssciname sblastname scomnames"
 
