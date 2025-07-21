62936d4b14f44a2c2998afb7567d42176c08431a
pascoalf <up202010522@g.uporto.pt>
2025-07-21 16:14:39 +0100

    update db

--- blastn/run_blast.sh
+++ blastn/run_blast.sh
@@ -1,3 +1,3 @@
 # Change path as necessary
-blastn -db ./db/mito -query ./ASV.fasta -out ../output/blast_results -outfmt "6 delim=, qacc qlen sseqid sacc slen evalue bitscore score length pident nident mismatch positive gaps staxid ssciname sblastname scomnames"
+blastn -db nt -query ASV_eDNA.fasta -out blast_results -outfmt "6 delim=, qacc qlen sseqid sacc slen evalue bitscore score length pident nident mismatch positive gaps staxid ssciname sblastname scomnames skingdoms" -remote
 
