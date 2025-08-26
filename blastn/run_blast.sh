27a0807399fab7f173c82bdd8f24495f2eb49a30
Luis-Afonso99 <afonsoluis7599gmail.com>
2025-08-26 11:42:01 +0100

    Add parameters to remote blastn

--- blastn/run_blast.sh
+++ blastn/run_blast.sh
@@ -1,3 +1,2 @@
 # Change path as necessary
-blastn -db nt -query ASV_eDNA.fasta -out blast_results -outfmt "6 delim=, qacc qlen sseqid sacc slen evalue bitscore score length pident nident mismatch positive gaps staxid ssciname sblastname scomnames skingdoms" -remote
-
+blastn -db nt -query ASV_atlantida.fasta -out blast_results_atlantida -outfmt "6 delim=, qacc qlen sseqid sacc slen evalue bitscore score length pident nident mismatch positive gaps staxid ssciname sblastname scomnames skingdoms" -evalue 1e-05 -perc_identity 99 -qcov_hsp_perc 80 -remote
