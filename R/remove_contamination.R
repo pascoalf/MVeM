118ccfeec3e216bcec536e1471fdfeb2e6aba2fe
pascoalf <up202010522@g.uporto.pt>
2025-10-17 10:53:03 +0100

    Add warning message and harmonize parameter conditions

--- R/remove_contamination.R
+++ R/remove_contamination.R
@@ -2,8 +2,17 @@
 # function to remove ASVs identified in the control respective to a sample
 remove_contamination <- function(data, sample, 
                                  map_sample = sample_control_map_long, 
-                                 threshold = NULL, option = "threshold", 
+                                 threshold = NULL, option = NULL, 
                                  output = "standard", ...){
+  # parameters warning
+  if(!is.null(option)){
+    if(!option %in% c("automatic", "threshold")){
+      stop("The option argument can be either 'automatic' or 'threshold'")
+    }
+  }
+  if(!output %in% c("standard", "contaminants")){
+    stop("The output argument can be either 'standard' or 'contaminants'")
+  }
   # make helper function to extract specific controls
   extract_controls <- function(x = sample_control_map_long,
                                asvs_original = data, 
@@ -36,13 +45,21 @@ remove_contamination <- function(data, sample,
 
   # Safe ASVs -- too abundant in original sample to be removed  
   # Treshold option
-  if(option == "threshold"){
+  if(is.null(option)){
+    data %>%
+      filter(Sample == sample) %>% 
+      filter(!ASV %in% asvs_in_control)
+    asvs_in_control.df <- data.frame(ASV = asvs_in_control)
+  } else if(option == "threshold"){
     safe_ASVs <- data %>% 
       filter(Sample == sample) %>%
       filter(Abundance >= threshold) %>% 
       pull(ASV) %>% 
       unique()
     safe_ASVs.df <- data.frame(ASV = safe_ASVs)    
+    # Remove safe ASVs from contaminant list
+    asvs_in_control.df <- data.frame(ASV = asvs_in_control) %>% 
+      anti_join(safe_ASVs.df, by = "ASV")
   } else if(option == "automatic"){
     # unsupervised clustering
     ucluster <- suppressWarnings(define_rb(data))
@@ -52,12 +69,10 @@ remove_contamination <- function(data, sample,
       pull(ASV) %>% 
       unique()
     safe_ASVs.df <- data.frame(ASV = safe_ASVs)
+    # Remove safe ASVs from contaminant list
+    asvs_in_control.df <- data.frame(ASV = asvs_in_control) %>% 
+      anti_join(safe_ASVs.df, by = "ASV")    
   }
-
-  # Remove safe ASVs from contaminant list
-  asvs_in_control.df <- data.frame(ASV = asvs_in_control) %>% 
-   anti_join(safe_ASVs.df, by = "ASV")
-  
 
   #
   if(output == "standard"){
