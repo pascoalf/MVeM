acdad8ee52afb0af5197820c8786eb0dd609758f
pascoalf <up202010522@g.uporto.pt>
2025-12-04 17:07:51 +0000

    Update assign_LCA

--- R/assign_LCA.R
+++ R/assign_LCA.R
@@ -8,28 +8,29 @@ assign_LCA <- function(x){
   genus_LCA <- x %>%  pull(Genus) %>% unique()
   species_LCA <- x %>%pull(Species) %>% unique()
   
+  taxonomic_levels <- c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species")
   #
   if(length(dom_LCA) > 1){
     LCA <- "Uncertain"
-    Level <- "Domain"
+    Level <- taxonomic_levels
   } else if(length(phyl_LCA) > 1){
     LCA <- dom_LCA
-    Level <- "Phylum"
+    Level <- taxonomic_levels[2:7]
   } else if(length(class_LCA) > 1){
     LCA <- phyl_LCA
-    Level <- "Class"
+    Level <- taxonomic_levels[3:7]
   } else if(length(ord_LCA) > 1){
     LCA <- class_LCA
-    Level <- "Order"
+    Level <- taxonomic_levels[4:7]
   } else if(length(fam_LCA) > 1){
     LCA <- ord_LCA
-    Level <- "Family"
+    Level <- taxonomic_levels[5:7]
   } else if(length(genus_LCA) > 1){
     LCA <- fam_LCA
-    Level <- "Genus"
+    Level <- taxonomic_levels[6:7]
   } else if(length(species_LCA) > 1){
     LCA <- paste(genus_LCA, "sp.")
-    Level <- "Species"
+    Level <- taxonomic_levels[7]
   } else {
     LCA <- species_LCA
     Level <- NA
